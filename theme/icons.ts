a1fbf82245a24b4ce28aa602fcafc0b92787c49d
Nico Martin <mail@nicomartin.ch>
2021-10-08 13:03:11 +0200

    added theme

---
+++ theme/icons.ts
@@ -0,0 +1,9 @@
+import twitter from './_icons/twitter.svg';
+
+const icons = {
+  twitter,
+};
+
+export type IconType = keyof typeof icons;
+
+export default icons;
---
+++ theme/index.ts
@@ -0,0 +1,9 @@
+export { default as SVG } from './svg/SVG';
+export { default as Icon } from './svg/Icon';
+export { default as Loader } from './misc/Loader';
+export { default as ShadowBox } from './shadowBox/ShadowBox';
+export { default as PortalBox } from './shadowBox/PortalBox';
+export { default as CloseButton } from './closeButton/CloseButton';
+
+export { default as icons } from './icons';
+export type { IconType } from './icons';
