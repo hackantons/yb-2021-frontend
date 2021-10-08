08119bae148334acc3a8b2a7405180f2088fdb09
Jonny Burger <jonathanburger11@gmail.com>
2021-10-08 12:46:53 +0200

    fassnacht demo video

--- remotion/videos/Goal.tsx
+++ remotion/videos/Goal.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { AbsoluteFill } from 'remotion';
+import { Spieler } from './Spieler';
+import texture from './texture.jpg';
+
+const container: React.CSSProperties = {
+  backgroundColor: '#222',
+};
+
+const background: React.CSSProperties = {
+  backgroundImage: `url(${texture})`,
+  opacity: 0.2,
+};
 
 export const Goal: React.FC = () => {
-  return <div>hi</div>;
+  return (
+    <AbsoluteFill style={container}>
+      <AbsoluteFill style={background}></AbsoluteFill>
+      <AbsoluteFill>
+        <Spieler></Spieler>
+      </AbsoluteFill>
+    </AbsoluteFill>
+  );
 };
