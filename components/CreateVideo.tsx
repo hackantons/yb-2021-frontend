1ee4830045e61210e5d0463b77f214b36341e046
Nico Martin <mail@nicomartin.ch>
2021-10-08 18:23:02 +0200

    added form styling

--- components/CreateVideo.tsx
+++ components/CreateVideo.tsx
@@ -3,22 +3,27 @@ import React, { useMemo } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 import { Form, FormElement, InputSelect, InputText } from '@theme';
 import cn from '@utils/classnames';
-import { PlayerI, TEAM_API } from '@utils/infos';
+import { PlayerI, TEAM_API, Teams } from '@utils/infos';
 import { Main } from '../remotion/videos/Main';
 import styles from './CreateVideo.module.css';
-import Download from './Download'
+import Download from './Download';
 
 const CreateVideo = ({ className = '' }: { className?: string }) => {
+  const filteredTeams = Object.values(Teams).filter((e) => e !== 'yb');
+  console.log('filteredTeams', filteredTeams, filteredTeams[0]);
   const form = useForm<{
     playerIndex: string;
     minute: number;
     homeScore: number;
     awayScore: number;
-    awayTeam: any;
+    awayTeam: Teams;
   }>({
     defaultValues: {
       playerIndex: '0',
       minute: 0,
+      homeScore: 1,
+      awayScore: 0,
+      awayTeam: filteredTeams[0],
     },
   });
 
@@ -49,6 +54,9 @@ const CreateVideo = ({ className = '' }: { className?: string }) => {
             lastName: selectedPlayer.lastName,
             seasonGoal: selectedPlayer.stat.goals,
             minute: formValues.minute,
+            homeScore: formValues.homeScore <= 1 ? 1 : formValues.homeScore,
+            awayScore: formValues.awayScore,
+            awayTeam: formValues.awayTeam,
           }}
         />
       </div>
@@ -77,6 +85,36 @@ const CreateVideo = ({ className = '' }: { className?: string }) => {
             label="Minute"
             Input={InputText}
             form={form}
+            type="number"
+          />
+          <FormElement
+            name="homeScore"
+            label="Score YB"
+            Input={InputText}
+            min="1"
+            form={form}
+            type="number"
+          />
+          <FormElement
+            name="awayScore"
+            label="Score Gast"
+            Input={InputText}
+            form={form}
+            type="number"
+            disabled
+          />
+          <FormElement
+            name="awayTeam"
+            label="Gegner"
+            Input={InputSelect}
+            form={form}
+            options={filteredTeams.reduce(
+              (acc, team) => ({
+                ...acc,
+                [team]: team,
+              }),
+              {}
+            )}
           />
         </Form>
         <Download />
