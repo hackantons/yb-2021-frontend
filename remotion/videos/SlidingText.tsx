e475e9e2caac21078a4419cd9c9c404d4ec2bf41
Jonny Burger <jonathanburger11@gmail.com>
2021-10-08 13:11:47 +0200

    build

---
+++ remotion/videos/SlidingText.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { interpolate, spring, useCurrentFrame, useVideoConfig } from 'remotion';
+
+export const SlidingText: React.FC<{
+  left: number;
+  top: number;
+  color: string;
+  fontSize: number;
+  delay: number;
+}> = ({ left, delay, top, color, children, fontSize }) => {
+  const frame = useCurrentFrame();
+  const { fps } = useVideoConfig();
+  const trans = spring({
+    fps,
+    frame: frame - delay,
+    config: {
+      mass: 0.5,
+      damping: 200,
+    },
+  });
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        height: fontSize * 0.87,
+        width: '100%',
+        left,
+        top,
+        overflow: 'hidden',
+      }}
+    >
+      <h1
+        style={{
+          color,
+          fontSize: fontSize,
+          fontFamily: 'Antique Olive Std',
+          lineHeight: 1,
+          marginTop: 0,
+          transform: `translateY(${interpolate(trans, [0, 1], [200, 0])}px)`,
+        }}
+      >
+        {children}
+      </h1>
+    </div>
+  );
+};
