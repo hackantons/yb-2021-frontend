8991210a50b51074f9d22e9de86a991f509edfb6
Jonas Niestroj <j.niestroj@protonmail.ch>
2021-10-08 16:28:11 +0200

    Add render and progress api

---
+++ pages/api/video/progress.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getRenderProgress } from '@remotion/lambda';
+
+type Data = {
+  overallProgress: number
+  outputFile: string
+}
+
+type RequestData = {
+  renderId: string
+  bucketName: string
+}
+
+const functionName = "remotion-render-fq3p205z2t"
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const body = req.body as RequestData
+  const progress = await getRenderProgress({
+    renderId: body.renderId,
+    bucketName: body.bucketName,
+    functionName,
+    region: 'eu-central-1'
+  })
+  res.status(200).json({ overallProgress: progress.overallProgress, outputFile: progress.outputFile })
+}
