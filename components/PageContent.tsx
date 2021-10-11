60ae4063799c2ef34afdcb1cc8eb0942d7742db1
Nico Martin <mail@nicomartin.ch>
2021-10-11 21:53:44 +0200

    started with password protected

--- components/PageContent.tsx
+++ components/PageContent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from '@comps/Navigation/Navigation';
+import { PasswordProtectedProvider } from '@comps/passwordProtected/PasswordProtectedProvider';
 import cn from '@utils/classnames';
 import styles from './PageContent.module.css';
 
@@ -14,12 +15,15 @@ const PageContent = ({
     <div className={cn(className, styles.root)}>
       <Navigation className={styles.navigation} />
       <div className={styles.content}>
-        <Main className={styles.main} />
+        <PasswordProtectedProvider>
+          <Main className={styles.main} />
+        </PasswordProtectedProvider>
         <footer className={styles.footer}>
           © 2021 hackantons -{' '}
           <a
             href="https://github.com/hackantons/yb-2021-frontend"
-            target="_blank" rel="noopener noreferrer"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Github
           </a>
